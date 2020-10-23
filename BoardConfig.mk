3dde07c714e027bf76cf2e516f95b265ed122426
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-10-23 09:35:08 +0200

    apollo: Inherit from common tree
    Change-Id: Icaf0e5cb7dbb8c75b81bf9008433ab51a9b34531

---
+++ BoardConfig.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from xiaomi sm8250-common
+-include device/xiaomi/sm8250-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/apollo
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := apollo,apollopro
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
+
+# Display
+TARGET_SCREEN_DENSITY := 440
+
+# Kernel
+TARGET_KERNEL_CONFIG := vendor/apollo_defconfig
+
+# Properties
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+
+# inherit from the proprietary version
+-include vendor/xiaomi/apollo/BoardConfigVendor.mk
