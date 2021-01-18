af7e4f1352d2997dac4d728d3481c0787c1415dd
ejbtrd <ejbtrd@gmail.com>
2021-01-18 21:26:08 +0000

    apollopro: Enable minimum refresh rate switch
    Signed-off-by: ejbtrd <ejbtrd@gmail.com>

--- device.mk
+++ device.mk
@@ -9,7 +9,8 @@ $(call inherit-product, device/xiaomi/sm8250-common/common.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
